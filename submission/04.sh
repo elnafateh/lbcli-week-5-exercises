c059ac4f9fae059c698859b70fe427539749f789
elnafateh <elnafatehh@gmail.com>
2026-07-22 23:34:41 +0100

    feat: 04 and 05

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,10 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
-# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+#publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+push_time="0420cd2459"
+op_cltv="b1"
+op_drop="75"
+script_p2pkh="76a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac"
+
+cltv_redeem_script="${push_time}${op_cltv}${op_drop}${script_p2pkh}"
+
+printf '%s' "$cltv_redeem_script"
--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,10 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+lock_sequence="029600"
+op_csv="b2"
+op_drop="75"
+script_p2pkh="76a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac"
+
+csv_redeem_script="${lock_sequence}${op_csv}${op_drop}${script_p2pkh}"
+
+printf '%s' "$csv_redeem_script"
